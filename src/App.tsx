8b5e609eee43f08113b00aee0d75282e31f03ae3
Marcos dos Santos <rrmarcosdossantos@gmail.com>
2024-02-29 22:11:19 -0300

    add react-router-dom and home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "../src/styles/themes/default";
 import { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router/Router";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>hello world!</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   );
