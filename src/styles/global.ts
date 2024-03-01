e7f2f7dca6692d01948a2f46a5bebf8a1bb0bfc4
Marcos dos Santos <rrmarcosdossantos@gmail.com>
2024-02-29 22:02:05 -0300

    initial project setup and add global style and theme

---
+++ src/styles/global.ts
@@ -0,0 +1,48 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+   * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: ${(props) => props.theme.white};
+    color: ${(props) => props.theme["blue-800"]};
+  }
+
+  h1, h2, h3 {
+    font-family: "Bebas Neue", sans-serif;
+    font-weight: 400;
+    text-transform: uppercase;
+    line-height: 1.5;
+    color: ${(props) => props.theme["blue-800"]};
+  }
+
+  h1 {
+    font-size: 3rem;
+  }
+
+  h2 {
+    font-size: 2rem;
+  }
+
+  h3 {
+    font-size: 1.5rem;
+  }
+
+  body, input, textarea, p {
+    font-family: "Montserrat", sans-serif;
+    font-weight: 400;
+    line-height: 1.5;
+  }
+
+  button, a {
+    font-family: "Bebas Neue", sans-serif;
+    font-weight: 400;
+    text-transform: uppercase;
+    font-size: 1.25rem;
+    color: ${(props) => props.theme["blue-800"]};
+  }
+`;
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,14 @@
+export const defaultTheme = {
+  white: "#FFFFFF",
+
+  gray: "#767B91",
+  yellow: "EBB726",
+
+  "blue-800": "#051A39",
+  "blue-600": "#0D4191",
+  "blue-400": "#44C3F0",
+  "blue-200": "#C9EDFB",
+
+  "red-400": "#F65C5C",
+  "red-200": "#FBC9C9",
+};
